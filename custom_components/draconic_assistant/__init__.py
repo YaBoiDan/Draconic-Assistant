71505c42332f7913b57b6bc755fcc05311abb5ec
Dan <ieras3rx@hotmail.co.uk>
2025-01-26 21:23:56 +0000

    Fixing await error in logs.

--- custom_components/draconic_assistant/__init__.py
+++ custom_components/draconic_assistant/__init__.py
@@ -7,10 +7,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry):
     hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
 
     # Forward the setup to the sensor platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
-    )
-
+    await hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, config_entry):
