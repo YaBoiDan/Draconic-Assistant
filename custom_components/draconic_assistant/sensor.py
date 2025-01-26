91361ec4a88c692dd3e6e4de446e5dbb063d39d9
Dan <ieras3rx@hotmail.co.uk>
2025-01-26 17:41:05 +0000

    Corrected sensors

--- custom_components/draconic_assistant/sensor.py
+++ custom_components/draconic_assistant/sensor.py
@@ -9,11 +9,19 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     entities = [
         CustomSensor("Status", device_id, "Unknown"),
         CustomSensor("Temperature", device_id, 0, unit_of_measurement="°C"),
-        CustomSensor("Saturation", device_id, 0, unit_of_measurement="%"),
-        CustomSensor("Field Strength", device_id, 0, unit_of_measurement="dB"),
-        CustomSensor("Fuel", device_id, 0, unit_of_measurement="%"),
-        CustomSensor("Input Energy", device_id, 0, unit_of_measurement="kWh"),
-        CustomSensor("Output Energy", device_id, 0, unit_of_measurement="kWh"),
+        CustomSensor("Saturation", device_id, 0),
+        CustomSensor("Max Saturation", device_id, 0),
+        #CustomSensor("Saturation Percentage", device_id, 0, unit_of_measurement="%"),
+        CustomSensor("Field Strength", device_id, 0),
+        CustomSensor("Max Field Strength", device_id, 0),
+        # CustomSensor("Field Strength Percentage", device_id, 0, unit_of_measurement="%"),
+        CustomSensor("Field Drain Rate", device_id, 0),
+        CustomSensor("Fuel Conversion", device_id, 0, unit_of_measurement="%"),
+        CustomSensor("Fuel Conversion Rate", device_id, 0),
+        CustomSensor("Field Drain Rate", device_id, 0),
+        CustomSensor("Failsafe", device_id, 0),
+        CustomSensor("Input Energy", device_id, 0, unit_of_measurement="RF/t"),
+        CustomSensor("Output Energy", device_id, 0, unit_of_measurement="RF/t"),
     ]
     async_add_entities(entities, update_before_add=True)
 
@@ -29,7 +37,7 @@ class CustomSensor(SensorEntity):
         return DeviceInfo(
             identifiers={(DOMAIN, self._device_id)},
             name="Draconic Reactor",
-            manufacturer="YaboiDan",
+            manufacturer="YaBoiDan",
             model="1.0",
             sw_version="1.0",
         )
